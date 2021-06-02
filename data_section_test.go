be3f3ca2099670c62d0dbdf81bf91b5a8a7e7011
Gregory Oschwald <goschwald@maxmind.com>
2021-06-02 09:08:53 -0700

    Add test that disabling pointers works in data writer

---
+++ data_section_test.go
@@ -0,0 +1,35 @@
+package mmdbwriter
+
+import (
+	"testing"
+
+	"github.com/maxmind/mmdbwriter/mmdbtype"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDisablingPointers(t *testing.T) {
+	v := mmdbtype.Slice{
+		mmdbtype.String("a repeated string"),
+		mmdbtype.String("a repeated string"),
+		mmdbtype.String("a repeated string"),
+		mmdbtype.String("a repeated string"),
+	}
+	dm := newDataMap()
+
+	key, err := dm.store(v)
+	require.NoError(t, err)
+
+	usePointers := true
+	pointerWriter := newDataWriter(dm, usePointers)
+
+	_, err = pointerWriter.maybeWrite(key)
+	require.NoError(t, err)
+
+	usePointers = false
+	noPointerWriter := newDataWriter(dm, usePointers)
+	_, err = noPointerWriter.maybeWrite(key)
+	require.NoError(t, err)
+
+	assert.Less(t, pointerWriter.Len(), noPointerWriter.Len())
+}
