6c6cdb066b388dd4f562266fc11d4d10eef6ad42
Gregory Oschwald <goschwald@maxmind.com>
2024-09-04 15:30:26 -0700

    Check node count before writing metadata

--- tree.go
+++ tree.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math"
 	"net"
 	"time"
 
@@ -649,6 +650,9 @@ func (t *Tree) writeMetadata(dw *dataWriter) (int64, error) {
 	for _, v := range t.languages {
 		languages = append(languages, mmdbtype.String(v))
 	}
+	if t.nodeCount > math.MaxUint32 {
+		return 0, fmt.Errorf("node count of %d exceeds the maximum allowed value", t.nodeCount)
+	}
 	metadata := mmdbtype.Map{
 		"binary_format_major_version": mmdbtype.Uint16(2),
 		"binary_format_minor_version": mmdbtype.Uint16(0),
@@ -658,6 +662,7 @@ func (t *Tree) writeMetadata(dw *dataWriter) (int64, error) {
 		//nolint:gosec // no risk. Will be 4 or 6.
 		"ip_version": mmdbtype.Uint16(t.ipVersion),
 		"languages":  languages,
+		//nolint:gosec // checked above
 		"node_count": mmdbtype.Uint32(t.nodeCount),
 		//nolint:gosec // no risk. Will be 24, 28, or 32.
 		"record_size": mmdbtype.Uint16(t.recordSize),
