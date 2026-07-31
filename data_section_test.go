0f4d3766dd3078b0a601f5e05a8ae273d6e79be2
Gregory Oschwald <goschwald@maxmind.com>
2026-07-31 22:07:40 +0000

    fixup! Redesign value storage around interned references

--- data_section_test.go
+++ data_section_test.go
@@ -105,6 +105,34 @@ func TestDataWriterRejectsOversizedOffset(t *testing.T) {
 	require.ErrorContains(t, err, "exceeds maximum")
 }
 
+func TestWriteContainerHeaderSizeBoundaries(t *testing.T) {
+	for _, test := range []struct {
+		name string
+		kind valueKind
+		size int
+		want []byte
+	}{
+		{"map inline", valueKindMap, 28, []byte{0xfc}},
+		{"map one byte start", valueKindMap, 29, []byte{0xfd, 0x00}},
+		{"map one byte end", valueKindMap, 284, []byte{0xfd, 0xff}},
+		{"map two byte start", valueKindMap, 285, []byte{0xfe, 0x00, 0x00}},
+		{"map two byte end", valueKindMap, 65820, []byte{0xfe, 0xff, 0xff}},
+		{"map three byte start", valueKindMap, 65821, []byte{0xff, 0x00, 0x00, 0x00}},
+		{"map three byte end", valueKindMap, 16843036, []byte{0xff, 0xff, 0xff, 0xff}},
+		{"slice extended type", valueKindSlice, 29, []byte{0x1d, 0x04, 0x00}},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			var output bytes.Buffer
+			require.NoError(t, writeContainerHeader(&output, test.kind, test.size))
+			assert.Equal(t, test.want, output.Bytes())
+		})
+	}
+
+	var output bytes.Buffer
+	err := writeContainerHeader(&output, valueKindMap, 16843037)
+	require.ErrorContains(t, err, "cannot store")
+}
+
 func TestSpoolReportsInvalidScratchPath(t *testing.T) {
 	spool := newSpool(t.TempDir() + "/missing")
 	spool.threshold = 1
